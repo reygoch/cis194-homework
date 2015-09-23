371b8f7373b33511586ddc38c236d436fb91da6b
Reygoch <reygoch@gmail.com>
2015-09-23 23:56:37 +0200

    Done with exercise 2, going to sleep ^^

--- HW04/HW04.hs
+++ HW04/HW04.hs
@@ -11,8 +11,11 @@ x = P [0,1]
 -- Exercise 2 ----------------------------------------
 
 instance (Num a, Eq a) => Eq (Poly a) where
-    (==) = undefined
- 
+    (==) (P a) (P b) = clean a == clean b
+        where
+            clean :: (Num a, Eq a) => [a] -> [a]
+            clean = reverse . dropWhile (==0) . reverse
+
 -- Exercise 3 -----------------------------------------
 
 instance (Num a, Eq a, Show a) => Show (Poly a) where
