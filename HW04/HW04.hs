f199f128df17f6039df62545466d1cc17b645b0c
Reygoch <reygoch@gmail.com>
2015-09-26 21:41:27 +0200

    Made full reset on HW04 because my approach was wrong from the beginning. I'm going to do some rethinking on how to make things more "type" flexible.

--- HW04/HW04.hs
+++ HW04/HW04.hs
@@ -6,44 +6,21 @@ newtype Poly a = P [a]
 -- Exercise 1 -----------------------------------------
 
 x :: Num a => Poly a
-x = P [0,1]
+x = undefined 
 
 -- Exercise 2 ----------------------------------------
 
 instance (Num a, Eq a) => Eq (Poly a) where
-    (==) (P a) (P b) = clean a == clean b
-        where
-            clean :: (Num a, Eq a) => [a] -> [a]
-            clean = reverse . dropWhile (==0) . reverse
+    (==) = undefined
 
 -- Exercise 3 -----------------------------------------
--- x^3 + 2x^2 + 1
 instance (Num a, Eq a, Show a) => Show (Poly a) where
-    show (P p) = creator 0 p
-        where
-            creator :: (Num a, Eq a, Show a) => Int -> [a] -> String
-            creator _ [] = ""
-            creator e [c] = part e c
-            creator e (0:cs) = creator (e+1) cs
-            creator e (c:cs) = creator (e+1) cs ++ " + " ++ part e c
-
-            part :: (Num a, Eq a, Show a) => Int -> a -> String
-            part 0 c = show c
-            part e 1 = "x^" ++ show e
-            part e (-1) = "-x^" ++ show e
-            part 1 c = show c ++ "x"
-            part e c = show c ++ "x^" ++ show e
+    show (P p) = undefined 
 
 -- Exercise 4 -----------------------------------------
 
 plus :: Num a => Poly a -> Poly a -> Poly a
-plus (P a) (P b) = P $ sumup a b
-    where
-        sumup :: Num a => [a] -> [a] -> [a]
-        sumup [] [] = []
-        sumup [] lb = lb
-        sumup la [] = la
-        sumup (la:las) (lb:lbs) = la + lb : sumup las lbs
+plus = undefined
 
 -- Exercise 5 -----------------------------------------
 
@@ -55,7 +32,7 @@ times = undefined
 instance Num a => Num (Poly a) where
     (+) = plus
     (*) = times
-    negate      = undefined
+    negate = undefined
     fromInteger = undefined
     -- No meaningful definitions exist
     abs    = undefined
