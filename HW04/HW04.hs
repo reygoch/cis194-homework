bf3af85c23465d24196893e5993de08e78b710da
Reygoch <reygoch@gmail.com>
2015-09-27 22:57:40 +0200

    Finally have some time to finish this homework.

--- HW04/HW04.hs
+++ HW04/HW04.hs
@@ -6,34 +6,61 @@ newtype Poly a = P [a]
 -- Exercise 1 -----------------------------------------
 
 x :: Num a => Poly a
-x = undefined 
+x = P [0,1]
 
 -- Exercise 2 ----------------------------------------
 
 instance (Num a, Eq a) => Eq (Poly a) where
-    (==) = undefined
+    (==) (P a) (P b) = clean a == clean b
 
 -- Exercise 3 -----------------------------------------
 instance (Num a, Eq a, Show a) => Show (Poly a) where
-    show (P p) = undefined 
+    show (P p) = if preppd /= [] then helper 0 preppd else "0"
+        where
+            preppd = clean p
+
+            helper :: (Num a, Eq a, Show a) => Int -> [a] -> String
+            helper _ [] = ""
+            helper e ([c]) = format e c 
+            helper e (0:cs) = helper (e + 1) cs
+            helper e (c:cs) = helper (e + 1) cs ++ " + " ++ format e c 
+
+            format :: (Num a, Eq a, Show a) => Int -> a -> String
+            format _ 0 = "0"
+            format 1 1 = "x"
+            format 0 c = show c
+            format 1 c = show c ++ "x" 
+            format e 1 = "x^" ++ show e
+            format e c = show c ++ "x^" ++ show e
 
 -- Exercise 4 -----------------------------------------
 
 plus :: Num a => Poly a -> Poly a -> Poly a
-plus = undefined
+plus (P a) (P b) = P $ helper a b
+    where
+        helper :: (Num a) => [a] -> [a] -> [a]
+        helper aa [] = aa
+        helper [] bb = bb
+        helper (aa:as) (bb:bs) = aa + bb : helper as bs
 
 -- Exercise 5 -----------------------------------------
 
 times :: Num a => Poly a -> Poly a -> Poly a
-times = undefined
+times (P a) (P b) = sum $ helper 0 a b
+    where
+        helper :: (Num a) => Int -> [a] -> [a] -> [Poly a] 
+        helper _ [] _ = [0]
+        helper c [aa] bs = [item c aa bs]
+        helper c (aa:as) bs = item c aa bs : helper (c + 1) as bs
+        item c aa bs = P $ replicate c 0 ++ map (*aa) bs
 
 -- Exercise 6 -----------------------------------------
 
 instance Num a => Num (Poly a) where
     (+) = plus
     (*) = times
-    negate = undefined
-    fromInteger = undefined
+    negate (P p) = P $ map (*(-1)) p 
+    fromInteger n = P [fromInteger n] 
     -- No meaningful definitions exist
     abs    = undefined
     signum = undefined
@@ -41,7 +68,11 @@ instance Num a => Num (Poly a) where
 -- Exercise 7 -----------------------------------------
 
 applyP :: Num a => Poly a -> a -> a
-applyP = undefined
+applyP (P p) n = helper 0 n p 
+    where
+        helper :: (Num a) => Int -> a -> [a] -> a
+        helper _ _ [] = 0
+        helper e xx (c:cs) = c*xx^e + helper (e + 1) xx cs 
 
 -- Exercise 8 -----------------------------------------
 
@@ -54,3 +85,7 @@ class Num a => Differentiable a where
 
 instance Num a => Differentiable (Poly a) where
     deriv = undefined
+
+-- Utilities ------------------------------------------
+clean :: (Eq a, Num a) => [a] -> [a]
+clean = reverse . dropWhile (==0) . reverse
