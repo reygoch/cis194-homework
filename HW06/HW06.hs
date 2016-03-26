23ea1c41d8452e3176c37190d7ecb35b1da4b0df
reygoch <reygoch@gmail.com>
2016-03-26 23:59:55 +0100

    done with HW06

--- HW06/HW06.hs
+++ HW06/HW06.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE BangPatterns #-}
 module HW06 where
 
 import Data.List
@@ -26,16 +27,16 @@ data Stream a = Cons a (Stream a)
 
 -- Show instance prints the first 20 elements followed by ellipsis
 instance Show a => Show (Stream a) where
-    show s = "[" ++ intercalate ", " (map show $ take 10 $ streamToList s)
-             ++ ",..."
+  show s = "[" ++ intercalate ", " (map show $ take 10 $ streamToList s)
+    ++ ",..."
 
-streamToList :: Stream a -> [a]
-streamToList (Cons val stream) = val : (streamToList stream)
+  streamToList :: Stream a -> [a]
+  streamToList (Cons val stream) = val : (streamToList stream)
 
 -- Exercise 4 -----------------------------------------
 
 instance Functor Stream where
-    fmap f (Cons val stream) = f val `Cons` (fmap f stream)
+  fmap f (Cons val stream) = f val `Cons` (fmap f stream)
 
 -- Exercise 5 -----------------------------------------
 
@@ -44,15 +45,15 @@ sRepeat x = x `Cons` (sRepeat x)
 
 sIterate :: (a -> a) -> a -> Stream a
 sIterate r s = s `Cons` helper r s
-	where
-		helper f x = f x `Cons` (helper f $ f x)
+  where
+    helper f x = f x `Cons` (helper f $ f x)
 
-sInterleave :: Stream a -> Stream a -> Stream a
-sInterleave (Cons v s1) s2 = v `Cons` (sInterleave s2 s1)
+    sInterleave :: Stream a -> Stream a -> Stream a
+    sInterleave (Cons v s1) s2 = v `Cons` (sInterleave s2 s1)
 
-sTake :: Int -> Stream a -> [a]
-sTake 0 _ = []
-sTake n (Cons v s) = v : sTake (n - 1) s
+    sTake :: Int -> Stream a -> [a]
+    sTake 0 _ = []
+    sTake n (Cons v s) = v : sTake (n - 1) s
 
 -- Exercise 6 -----------------------------------------
 
@@ -66,25 +67,34 @@ ruler = sRepeat 0 `sInterleave` (sRepeat 1 `sInterleave` (sRepeat 2 `sInterleave
 
 -- | Implementation of C rand
 rand :: Int -> Stream Int
-rand = undefined
+rand = sIterate (\x -> 1103515245 * x + 12345 `mod` 2147483648) 
 
 -- Exercise 8 -----------------------------------------
 
-{- Total Memory in use: ??? MB -}
+{- Total Memory in use: ~13 MB -}
 minMaxSlow :: [Int] -> Maybe (Int, Int)
 minMaxSlow [] = Nothing   -- no min or max if there are no elements
 minMaxSlow xs = Just (minimum xs, maximum xs)
 
 -- Exercise 9 -----------------------------------------
 
-{- Total Memory in use: ??? MB -}
+{- Total Memory in use: ~6 KB -} -- this shit is raaaad!
 minMax :: [Int] -> Maybe (Int, Int)
-minMax = undefined
+minMax [] = Nothing
+minMax (x:xs) = Just $ helper xs x x
+  where
+    helper [] mn mx = (mn, mx)
+    helper (x:xs) !mn !mx = helper xs (min x mn) (max x mx)
 
-main :: IO ()
-main = print $ minMaxSlow $ sTake 1000000 $ rand 7666532
+    main :: IO ()
+    main = print $ minMax $ sTake 1000000 $ rand 7666532
 
 -- Exercise 10 ----------------------------------------
+data Matrix n = Mx {a :: n, b :: n, c :: n,  d :: n} deriving (Show)
+
+instance (Num n) => Num (Matrix n) where
+	(*) (Mx a b c d) (Mx e f g h) = Mx (a*e + b*g) (a*f + b*h) (c*e + d*g) (c*f + d*h)
 
 fastFib :: Int -> Integer
-fastFib = undefined
+fastFib 0 = 1
+fastFib n = a $ (Mx 1 1 1 0) ^ n
