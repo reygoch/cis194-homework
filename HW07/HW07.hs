1c73b8b207831771508106c54552b4bbf7cdfffa
reygoch <reygoch@gmail.com>
2016-03-29 18:29:05 +0200

    finished HW07

--- HW07/HW07.hs
+++ HW07/HW07.hs
@@ -21,40 +21,51 @@ liftM :: Monad m => (a -> b) -> m a -> m b
 liftM f mv = mv >>= return . f
 
 swapV :: Int -> Int -> Vector a -> Maybe (Vector a)
-swapV = undefined
+swapV i1 i2 v = liftM2
+  (\v1 v2 -> v // [(i1, v2), (i2, v1)]) (v V.!? i1) (v V.!? i2)
 
 -- Exercise 2 -----------------------------------------
 
 mapM :: Monad m => (a -> m b) -> [a] -> m [b]
-mapM = undefined
+mapM f = sequence . map f
 
 getElts :: [Int] -> Vector a -> Maybe [a]
-getElts = undefined
+getElts is v = mapM (v!?) is
 
 -- Exercise 3 -----------------------------------------
 
 type Rnd a = Rand StdGen a
 
 randomElt :: Vector a -> Rnd (Maybe a)
-randomElt = undefined
+randomElt v = getRandomR (0, V.length v - 1) >>= return . (v !?)
 
 -- Exercise 4 -----------------------------------------
 
 randomVec :: Random a => Int -> Rnd (Vector a)
-randomVec = undefined
+randomVec n = V.fromList <$!> replicateM n getRandom
 
+-- just for the sake of variety :D
 randomVecR :: Random a => Int -> (a, a) -> Rnd (Vector a)
-randomVecR = undefined
+randomVecR 0 _ = return V.empty
+randomVecR n b = liftM2 cons (getRandomR b) (randomVec (n - 1))
 
 -- Exercise 5 -----------------------------------------
+swap :: (Int, Int) -> Vector a -> Vector a
+swap (i, j) v = v // [(i, v ! j), (j, v ! i)]
 
 shuffle :: Vector a -> Rnd (Vector a)
-shuffle = undefined
+shuffle v = forM [n-1, n-2..1] genSwap >>= return . foldr swap v
+  where
+    n = V.length v
+    genSwap i = (\j -> (i, j)) <$> getRandomR (0, i)
 
 -- Exercise 6 -----------------------------------------
 
 partitionAt :: Ord a => Vector a -> Int -> (Vector a, a, Vector a)
-partitionAt = undefined
+partitionAt v i = (below, p, above)
+  where
+    p = v ! i
+    (below, above) = V.unstablePartition (< p) $ V.tail $ swap (0, i) v
 
 -- Exercise 7 -----------------------------------------
 
@@ -65,36 +76,63 @@ quicksort (x:xs) = quicksort [ y | y <- xs, y < x ]
                    <> (x : quicksort [ y | y <- xs, y >= x ])
 
 qsort :: Ord a => Vector a -> Vector a
-qsort = undefined
+qsort v
+  | V.null v = V.empty
+  | otherwise = qsort below <> (p `cons` qsort above)
+  where
+    (below, p, above) = partitionAt v 0
 
 -- Exercise 8 -----------------------------------------
 
 qsortR :: Ord a => Vector a -> Rnd (Vector a)
-qsortR = undefined
+qsortR v
+  | V.null v = return V.empty
+  | otherwise = parts >>= mergeSort
+  where
+    parts = getRandomR (0, V.length v - 1) >>= return . partitionAt v
+    mergeSort (b, p, t) = liftM2 (<>) (qsortR b) (liftM (cons p) (qsortR t))
+
 
 -- Exercise 9 -----------------------------------------
 
 -- Selection
 select :: Ord a => Int -> Vector a -> Rnd (Maybe a)
-select = undefined
+select i v
+  | V.null v = return Nothing
+  | otherwise = liftM (partitionAt v) rpi >>= helper
+  where
+    rpi = getRandomR (0, V.length v - 1)
+    helper (bottom, pivot, top)
+      | i < blen = select i bottom
+      | i > blen = select (i - blen - 1) top
+      | otherwise = return $ Just pivot
+      where blen = V.length bottom
 
 -- Exercise 10 ----------------------------------------
 
 allCards :: Deck
-allCards = undefined
+allCards = [Card label suit | suit <- suits, label <- labels]
 
 newDeck :: Rnd Deck
-newDeck =  undefined
+newDeck =  shuffle allCards
 
 -- Exercise 11 ----------------------------------------
 
 nextCard :: Deck -> Maybe (Card, Deck)
-nextCard = undefined
+nextCard d
+  | V.null d = Nothing
+  | otherwise = Just (V.head d, V.tail d)
 
 -- Exercise 12 ----------------------------------------
 
 getCards :: Int -> Deck -> Maybe ([Card], Deck)
-getCards = undefined
+getCards n = helper 0 []
+  where
+    helper :: Int -> [Card] -> Deck -> Maybe ([Card], Deck)
+    helper 0 cs dick = return (cs, dick)
+    helper i cs dick = do
+      (c, d) <- nextCard dick
+      helper (n + 1) (c:cs) d
 
 -- Exercise 13 ----------------------------------------
 
